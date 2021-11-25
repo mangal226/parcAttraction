79d9dc5683cd269fa2f5cfc3e09a96ff4416a563
Hamed <mangali226@yahoo.fr>
2021-11-25 17:10:07 +0100

    Add_heritance

--- src/main/java/model/Caissier.java
+++ src/main/java/model/Caissier.java
@@ -1,9 +1,13 @@
 package model;
+import javax.persistence.Entity;
 
+@Entity
 public class Caissier extends Compte {
 	
 	public Caissier(int id, String login, String password) {
 		super(id, login, password);
+	}
+	public Caissier() {
 	}
     
 
--- src/main/java/model/Gerant.java
+++ src/main/java/model/Gerant.java
@@ -1,8 +1,10 @@
 package model;
 
 import java.util.ArrayList;
+import javax.persistence.Entity;
 import java.util.List;
 
+@Entity
 public class Gerant extends Compte{
 
 
@@ -12,6 +14,10 @@ public class Gerant extends Compte{
 	public Gerant(int id, String login, String password) {
 		super(id, login, password);
 	}
+	
+	public Gerant() {
+	}
+	
 
 	public List<Famille> getListeFamilleAyantVisite() {
 		return listeFamilleAyantVisite;
