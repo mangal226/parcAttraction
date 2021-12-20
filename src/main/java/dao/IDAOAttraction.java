a841ae70835acb04cd82462041cc5c99d1b092a4
QuentinPonchon <ponchonquentin@gmail.com>
2021-12-20 13:06:37 +0100

    Suppression DAO + changement pom xml

--- src/main/java/dao/IDAOAttraction.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-import model.Attraction;
-
-public interface IDAOAttraction extends IDAO<Attraction,Integer>{
-
-}
--- src/test/java/test/jupiter/AttractionTest.java
+++ src/test/java/test/jupiter/AttractionTest.java
@@ -2,8 +2,20 @@ package test.jupiter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import javax.transaction.Transactional;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import config.AppConfig;
+
+@Transactional
+@Rollback(true)
+@ExtendWith(SpringExtension.class) // remplace @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { AppConfig.class })
 class AttractionTest {
 
 	@Test
