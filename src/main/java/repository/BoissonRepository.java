077008975817d8f3f8723d6ae27171ca060d35d6
QuentinPonchon <ponchonquentin@gmail.com>
2021-12-20 11:50:39 +0100

    Commit Boisson et Attraction

---
+++ src/main/java/repository/BoissonRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Boisson;
+
+public interface BoissonRepository extends JpaRepository <Boisson, Long>{
+
+}
