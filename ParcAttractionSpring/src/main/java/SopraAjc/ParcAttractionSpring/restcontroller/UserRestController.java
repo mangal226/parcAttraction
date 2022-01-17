75b4c437714edbc7ea504eea751eb138818a2172
Hamed <mangali226@yahoo.fr>
2022-01-17 10:43:31 +0100

    UserRestController

---
+++ ParcAttractionSpring/src/main/java/SopraAjc/ParcAttractionSpring/restcontroller/UserRestController.java
@@ -0,0 +1,45 @@
+package SopraAjc.ParcAttractionSpring.restcontroller;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import SopraAjc.ParcAttractionSpring.model.JsonViews;
+import SopraAjc.ParcAttractionSpring.model.Role;
+import SopraAjc.ParcAttractionSpring.model.User;
+import SopraAjc.ParcAttractionSpring.repository.UserRepository;
+
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/user")
+public class UserRestController {
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	@Autowired
+	UserRepository userRepo;
+	
+	@PostMapping("/inscription")
+	@JsonView(JsonViews.Common.class)
+	public User inscription(@RequestBody User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		user.setRoles(Arrays.asList(Role.ROLE_ADMIN));
+		user.setEnable(true);
+		return userRepo.save(user);
+	}
+	
+	@GetMapping("")
+	public boolean login() {
+		return true;
+	}
+}
