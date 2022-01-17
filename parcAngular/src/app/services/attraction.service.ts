feea217e7d69e243dc2ba7a6601bf02a5c44e673
clement <clementloiz@sfr.fr>
2022-01-17 16:33:45 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ parcAngular/src/app/services/attraction.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { AuthenticationService } from './authentification.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Boutique } from '../model/attraction';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AtttractionService {
+
+  private static URL: string = 'http://localhost:8080/lotr/api/attraction';
+  constructor(private http: HttpClient, private auth: AuthenticationService) { }
+
+  public getAll(): Observable<Attraction[]> {
+    return this.http.get<Attraction[]>(AttractionService.URL, {
+      headers: this.auth.headers,
+    });
+  }
+
+
+  public getById(id: number): Observable<Attraction> {
+    return this.http.get<Attraction>(AttractionService.URL + '/' + id, {
+      headers: this.auth.headers,
+    });
+  }
+
+  public update(attraction: Attraction): Observable<Attraction> {
+    return this.http.put<Attraction>(
+      AttractionService.URL + '/' + attraction.id,
+      attraction,
+      {
+        headers: this.auth.headers,
+      }
+    );
+  }
+
+  public create(attraction: Attraction): Observable<Attraction> {
+    const o = {
+      nom: attraction.nom,
+    };
+    return this.http.post<Attraction>(AttractionService.URL, o, {
+      headers: this.auth.headers,
+    });
+  }
+
+  public delete(id: number): Observable<void> {
+    return this.http.delete<void>(AttractionService.URL + '/' + id, {
+      headers: this.auth.headers,
+    });
+  }
+}
