47119ea044c080bf6da18e3aaa17913b4ba2f0e0
Hamed <mangali226@yahoo.fr>
2022-01-17 15:26:30 +0100

    modelBoutique

--- parcAngular/src/app/model/boutique.ts
+++ parcAngular/src/app/model/boutique.ts
@@ -1,2 +1,21 @@
+import { Marchandise } from "./marchandise";
+
 export class Boutique {
+    public constructor(private _id?: number, private _nom?: string, private marchandise?: Marchandise[]){
+
+    }
+    public get id(): number | undefined {
+      return this._id;
+    }
+
+    public set id(value: number | undefined) {
+      this._id = value;
+    }
+    public get nom(): string | undefined {
+      return this._nom;
+    }
+
+    public set nom(value: string | undefined) {
+      this._nom = value;
+    }
 }
