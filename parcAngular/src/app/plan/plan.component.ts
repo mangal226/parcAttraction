4d08b6ed1b4908f8b1c5896570d7689aa760ed87
QuentinPonchon <ponchonquentin@gmail.com>
2022-01-18 16:53:37 +0100

    plan parc OK

--- parcAngular/src/app/plan/plan.component.ts
+++ parcAngular/src/app/plan/plan.component.ts
@@ -31,16 +31,24 @@ export class PlanComponent implements OnInit {
     private attractionService: AttractionService
   ) {}
   ngOnInit(): void {
-    this.attractions.subscribe() = this.attractionService.getAll();
-    this.boutiques = this.boutiqueService.getAll();
+    this.attractionService.getAll().subscribe((result) => {
+      this.attractionsLocal = result;
+    });
+    this.boutiqueService.getAll().subscribe((result) => {
+      this.boutiquesLocal = result;
+    });
+    this.restaurationService.getAll().subscribe((result) => {
+      this.restaurationsLocal = result;
+      this.coordonneesUtilisees();
+    });
 
-    this.restaurations = this.restaurationService.getAll();
-    this.coordonneesUtilisees();
+    // this.boutiques = this.boutiqueService.getAll();
+    // this.restaurations = this.restaurationService.getAll();
+    // this.attractions=this.attractionService.getAll();
   }
 
   checkVide(a: number, b: number) {
     let coordonneesLocal: Coordonnees = new Coordonnees(a, b);
-    console.log();
     if (
       this.coordonneesUtilisesLocal.findIndex(
         (choix) =>
@@ -53,36 +61,61 @@ export class PlanComponent implements OnInit {
   }
 
   coordonneesUtilisees() {
-    this.boutiques!.subscribe((boutique) => {
-      for (let index in boutique) {
-        this.coordonneesUtilisesLocal.push(boutique[index].coordonnees!);
-      }
-    });
-    this.restaurations!.subscribe((restau) => {
-      for (let index2 in restau) {
-        this.coordonneesUtilisesLocal.push(restau[index2].coordonnees!);
-      }
-    });
-    this.attractions!.subscribe((attraction) => {
-      for (let index3 in attraction) {
-        this.coordonneesUtilisesLocal.push(attraction[index3].coordonnees!);
-      }
-    });
+    for (let index in this.boutiquesLocal) {
+      this.coordonneesUtilisesLocal.push(
+        this.boutiquesLocal[index].coordonnees!
+      );
+    }
+    for (let index in this.attractionsLocal) {
+      this.coordonneesUtilisesLocal.push(
+        this.attractionsLocal[index].coordonnees!
+      );
+    }
+    for (let index in this.restaurationsLocal) {
+      this.coordonneesUtilisesLocal.push(
+        this.restaurationsLocal[index].coordonnees!
+      );
+    }
+    // this.boutiques!.subscribe((boutique) => {
+    //   for (let index in boutique) {
+    //     this.coordonneesUtilisesLocal.push(boutique[index].coordonnees!);
+    //   }
+    // });
+    // this.restaurations!.subscribe((restau) => {
+    //   for (let index2 in restau) {
+    //     this.coordonneesUtilisesLocal.push(restau[index2].coordonnees!);
+    //   }
+    // });
+    // this.attractions!.subscribe((attraction) => {
+    //   for (let index3 in attraction) {
+    //     this.coordonneesUtilisesLocal.push(attraction[index3].coordonnees!);
+    //   }
+    // });
   }
 
   checkBoutique(a: number, b: number) {
     let coordonneesLocal: Coordonnees = new Coordonnees(a, b);
-    this.boutiques!.subscribe((sub) => {
-      console.log(sub);
-      // for (let index in sub) {
-      //   if (
-      //     sub[index].coordonnees!.x == coordonneesLocal.x &&
-      //     sub[index].coordonnees!.y == coordonneesLocal.y
-      //   ) {
-      //     return true;
-      //   }
-      // }
-      return false;
-    });
+    for (let index in this.boutiquesLocal) {
+      if (
+        this.boutiquesLocal[index].coordonnees!.x == coordonneesLocal.x &&
+        this.boutiquesLocal[index].coordonnees!.y == coordonneesLocal.y
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkAttraction(a: number, b: number) {
+    let coordonneesLocal: Coordonnees = new Coordonnees(a, b);
+    for (let index in this.attractionsLocal) {
+      if (
+        this.attractionsLocal[index].coordonnees!.x == coordonneesLocal.x &&
+        this.attractionsLocal[index].coordonnees!.y == coordonneesLocal.y
+      ) {
+        return true;
+      }
+    }
+    return false;
   }
 }
